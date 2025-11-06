31502e922f4d5413af5175ca16e718f57c0bbe29
Paco16889 <francisco.cobsan@gmail.com>
2025-11-06 23:32:23 +0100

    comienzo a construir el login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+ private router:Router = inject(Router);
+  readonly navigateTo:string = "";
+
+
+  constructor(){
+    //sacar el formlogind el formbuilder 
+  }
+
+  //metodo onsubmit()
+  getError(control:string){
+       
+    switch(control){
+      case 'email':
+        if(this.formLogin.controls.email.errors!=null && 
+           Object.keys(this.formLogin.controls.email.errors).includes('required'))
+           return "El campo email es requerido";
+        else if(this.formLogin.controls.email.errors!=null && 
+           Object.keys(this.formLogin.controls.email.errors).includes('email'))
+           return "El email no es correcto";
+        
+        break;
+      case 'password': 
+        if(this.formLogin.controls.password.errors!=null && 
+           Object.keys(this.formLogin.controls.password.errors).includes('required'))
+           return "El campo email es requerido";
+        break;
+      default:return "";
+    }
+    return "";
+  }
+
+}
+
 
 }
