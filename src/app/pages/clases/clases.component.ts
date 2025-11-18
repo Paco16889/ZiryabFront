1556c424476a7f3435e556dcaf70e4f7b936fcfd
Paco16889 <francisco.cobsan@gmail.com>
2025-11-18 19:24:34 +0100

    crear componente subject

--- src/app/pages/clases/clases.component.ts
+++ src/app/pages/clases/clases.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SubjectComponent } from '../subject/subject.component';
 
 @Component({
   selector: 'app-clases',
-  imports: [],
+  imports: [SubjectComponent],
   templateUrl: './clases.component.html',
   styleUrl: './clases.component.scss'
 })
