ef4fdffacc41624edc3827e4383390fbfd54c306
angela1006 <amormat1010@g.educaand.es>
2025-11-18 19:12:39 +0100

    modificar gestion y temario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './core/guards/auth.guard';
 import { ClasesComponent } from './pages/clases/clases.component';
 import { GestionComponent } from './pages/gestion/gestion.component';
+import { TemarioComponent } from './temario/temario.component';
 
 export const routes: Routes = [
     {
@@ -27,5 +28,9 @@ export const routes: Routes = [
     {
         path:'gestion',
         component:GestionComponent
+    },
+    {
+        path:'temario',
+        component:TemarioComponent
     }
 ];
