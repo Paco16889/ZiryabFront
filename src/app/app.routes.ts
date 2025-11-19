82e5eb173ba4c8aa0d19d47980f837a434c8a1bb
angela1006 <amormat1010@g.educaand.es>
2025-11-19 19:59:58 +0100

    modificar componentes html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -30,7 +30,7 @@ export const routes: Routes = [
         component:GestionComponent
     },
     {
-        path:'temario',
+        path:'temario/:claseId',
         component:TemarioComponent
     }
 ];
--- src/app/pages/clases/clases.component.ts
+++ src/app/pages/clases/clases.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { NavigationService } from '../../core/services/navigation.service'; 
+
 
 @Component({
   selector: 'app-clases',
-  imports: [],
+  imports: [CommonModule], 
   templateUrl: './clases.component.html',
   styleUrl: './clases.component.scss'
 })
 export class ClasesComponent {
-
+  constructor(private navegador: NavigationService) {}
+  
+  goToTemario(clase: string){
+    this.navegador.toComponent(`temario/${clase.toLowerCase()}`); 
+  }
 }
