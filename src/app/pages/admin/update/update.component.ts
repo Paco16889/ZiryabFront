78bfbf089034aca43d21272832fd969356872782
Paco16889 <francisco.cobsan@gmail.com>
2025-11-30 12:12:50 +0100

    simulacion de busqueda ficticia de alumno

--- src/app/pages/admin/update/update.component.ts
+++ src/app/pages/admin/update/update.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update',
-  imports: [],
-  templateUrl: './update.component.html',
-  styleUrl: './update.component.scss'
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './update.component.html'
 })
 export class UpdateComponent {
 
+  // Formulario reactivo
+  formSearch: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Inicializamos el formulario con un solo control: criteria
+    this.formSearch = this.fb.group({
+      criteria: ['']  // valor inicial vacío
+    });
+  }
+  showResult = false;  // controla si se muestra el bloque
+  studentResult = {
+  email: 'ejemplo@email.com',
+  name: 'Juan',
+  surname: 'Pérez',
+  ndSurname: 'Gómez',
+  fechaNacimiento: '2000-01-01',
+  dni: '12345678A'
+  };
+
+  // onSubmit simple: solo muestra el criterio seleccionado
+  onSubmit() {
+    if (this.formSearch.invalid) return;
+    this.showResult = true;
+      //aqui deberia llamar al servicio que busca por el criterio selecionado lanzando el tipo de criterio y el valor
+    console.log('Criterio seleccionado:', this.formSearch.value.criteria);
+  }
 }
-//ESTAMOS EN LA RAMA DE UPDATE PARA LUEGO FUSIONAR CON LA "PRINCIPAL"
-//SOLO VAMOS A TOCAR ESTE COMPONENTE DE MOMENTO
