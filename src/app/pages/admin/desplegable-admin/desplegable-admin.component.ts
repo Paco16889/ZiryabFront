acc1cb38200a19ab2ce2c49475972d7c3e9102ac
Paco16889 <francisco.cobsan@gmail.com>
2025-12-04 21:18:06 +0100

    creación de tarjeta de alumno

--- src/app/pages/admin/desplegable-admin/desplegable-admin.component.ts
+++ src/app/pages/admin/desplegable-admin/desplegable-admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ToggleService } from '../../../core/services/toggle.service';
 import { FilterSectionComponent } from '../filter-section/filter-section.component';
+import { ListItemComponent } from '../list-item/list-item.component';
 
 @Component({
   selector: 'app-desplegable-admin',
-  imports: [],
+  imports: [ListItemComponent],
   templateUrl: './desplegable-admin.component.html',
   styleUrl: './desplegable-admin.component.scss'
 })
