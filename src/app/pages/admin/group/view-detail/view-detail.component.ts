81d6e71e6104d70a6b9b393c6ca8d740e1b1a52d
Paco16889 <francisco.cobsan@gmail.com>
2025-12-11 07:44:45 +0100

    componentes de lista

---
+++ src/app/pages/admin/group/view-detail/view-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-detail',
+  imports: [],
+  templateUrl: './view-detail.component.html',
+  styleUrl: './view-detail.component.scss'
+})
+export class ViewDetailComponent {
+
+}
