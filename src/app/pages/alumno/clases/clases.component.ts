763531c224a037a1e7a0570d6bccd90ba1267fff
angela1006 <amormat1010@g.educaand.es>
2025-12-14 16:54:09 +0100

    añadir cambios en clases

--- src/app/pages/alumno/clases/clases.component.ts
+++ src/app/pages/alumno/clases/clases.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { NavigationService } from '../../../core/services/navigation.service'; 
 import { ClasesService } from '../../../core/services/clases.service';
@@ -11,46 +11,48 @@ import { LocalStorageAuthService } from '../../../core/services/localstorage-aut
   templateUrl: './clases.component.html',
   styleUrl: './clases.component.scss'
 })
-export class ClasesComponent {
+export class ClasesComponent implements OnInit{
   private navegador = inject(NavigationService);
   private clasesService=inject(ClasesService);
   private authService=inject(LocalStorageAuthService);
 
-  public asignatura=signal<any[]>([]);
+  public asignaturas=signal<any[]>([]);
   public loading=signal<boolean>(true);
+  public errorMessage = signal<string>(''); 
+
 
   public colorThemes = [
-    { // Azul
+    { 
       bg: 'from-blue-100 via-blue-50 to-blue-200 border-blue-200',
       line: 'border-blue-300',
       text: 'text-blue-700',
       btn: 'bg-blue-500 hover:bg-blue-600'
     },
-    { // Verde
+    { 
       bg: 'from-green-100 via-green-50 to-green-200 border-green-200',
       line: 'border-green-300',
       text: 'text-green-700',
       btn: 'bg-green-500 hover:bg-green-600'
     },
-    { // Naranja
+    { // 
       bg: 'from-orange-100 via-orange-50 to-orange-200 border-orange-200',
       line: 'border-orange-300',
       text: 'text-orange-700',
       btn: 'bg-orange-500 hover:bg-orange-600'
     },
-    { // Amarillo
+    { // 
       bg: 'from-yellow-100 via-yellow-50 to-yellow-200 border-yellow-200',
       line: 'border-yellow-300',
       text: 'text-yellow-700',
       btn: 'bg-yellow-500 hover:bg-yellow-600'
     },
-    { // Rosa
+    { // 
       bg: 'from-pink-100 via-pink-50 to-pink-200 border-pink-200',
       line: 'border-pink-300',
       text: 'text-pink-700',
       btn: 'bg-pink-500 hover:bg-pink-600'
     },
-    { // Zinc/Gris
+    { 
       bg: 'from-zinc-100 via-zinc-50 to-zinc-200 border-zinc-200',
       line: 'border-zinc-300',
       text: 'text-zinc-700',
@@ -65,22 +67,29 @@ export class ClasesComponent {
       this.clasesService.getAsignaturaAlumno(user.id).subscribe({
         next:(data)=>{
           console.log('Asignaturas recibidas:', data);
-          this.asignatura.set(data);
+          if (data.length === 0) {
+            this.errorMessage.set('No tienes asignaturas matriculadas.');
+          }
+          this.asignaturas.set(data);
           this.loading.set(false)
         },
         error:(err)=>{
           console.error('Error al cargar asignatura: ',err);
-          this.loading.set(false)
+          this.errorMessage.set('Error de conexión con el servidor.');
+          this.loading.set(false);
         }
       });
     }else{
       console.warn('No se encontro usuario logueado.');
+      this.errorMessage.set('Usuario no identificado. Por favor, inicia sesión.');
       this.loading.set(false);
       this.navegador.toComponent('login');
     }
   }
   
   goToTemario(nombreAsignatura: string):void{
-    this.navegador.toComponent(`temario/${nombreAsignatura.toLowerCase()}`); 
+    if (nombreAsignatura) {
+      this.navegador.toComponent(`temario/${nombreAsignatura.toLowerCase()}`); 
+    }  
   }
 }
