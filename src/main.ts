551f90f08f0a54eb291e42a0bafc04bf779f1dcc
Paco16889 <francisco.cobsan@gmail.com>
2026-02-27 00:51:41 +0100

    comenta resto ficheros

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+
+/**
+ * Punto de entrada de la aplicación.
+ * Inicializa la aplicación Angular con el componente raíz y la configuración global.
+ */
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
