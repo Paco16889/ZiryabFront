b2df0407995ea9d43c37cf36b29613e11d7b24ee
Antonio Salces Alcaraz <antoniosalces06@gmail.com>
2026-03-29 20:58:51 +0200

    Auditar ToggleService (Issue #10)

--- src/app/core/services/toggle.service.spec.ts
+++ src/app/core/services/toggle.service.spec.ts
@@ -13,4 +13,25 @@ describe('ToggleService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have initial state null', () => {
+    expect(service.openedMenu()).toBeNull();
+  });
+
+  it('should open a menu when toggled for the first time', () => {
+    service.toggle('students');
+    expect(service.openedMenu()).toBe('students');
+  });
+
+  it('should close the menu when toggling the currently open menu', () => {
+    service.toggle('students'); // Abre
+    service.toggle('students'); // Cierra
+    expect(service.openedMenu()).toBeNull();
+  });
+
+  it('should switch menu when toggling a different menu', () => {
+    service.toggle('students');
+    service.toggle('teachers');
+    expect(service.openedMenu()).toBe('teachers');
+  });
 });
